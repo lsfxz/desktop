ebac6b746ff96ee689652bb8e00974baf36ea0a4
lsfxz <1813007-ohfp@users.noreply.gitlab.com>
2021-09-25 18:42:17 +0200

    enable building with electron 13

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,11 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = (env) =>
-  common(env).map((config) =>
+  common({
+    ...env,
+    onlyTranspileTypescript: true,
+    experimentalFeatures: true,
+  }).map((config) =>
     merge(config, {
       mode: 'production',
       devtool: 'source-map',
